599c3d6d90aa03c771cbc0055407b52adef98705
alina <khomkolovaalina@gmail.com>
2025-03-19 16:57:13 +0200

    Рефакторинг: переписана логика создания таблиц с использованием SQLAlchemy ORM вместо обычных SQL-выражений.

---
+++ telegram_bot/queries/engine.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from config import settings
+from telegram_bot.queries.models import Base
+
+engine = create_async_engine(settings.database_url_asyncpg, echo=True)
+
+session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+async def create_table():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def drop_table():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
