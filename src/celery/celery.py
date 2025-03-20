c3e436ed58fd3c2c109c379c97c95380474e9562
alina <khomkolovaalina@gmail.com>
2025-03-20 16:12:27 +0200

    исправила название задачи

--- src/celery/celery.py
+++ src/celery/celery.py
@@ -5,7 +5,7 @@ app = Celery('tasks', broker='redis://localhost:6379/0')
 
 # Настройка периодической задачи
 app.conf.beat_schedule = {
-    'fetch-codeforces-every-minute': {
+    'fetch-codeforces-every-hour': {
         'task': 'src.celery.tasks.fetch_codeforces_data',
         'schedule': crontab(minute="0", hour="*"),  # Каждые 1 час (в начале часа)
     },
